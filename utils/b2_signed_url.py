f4810a67d9b757a8c2d2adb36d2babd1967a45b8
ElegbonH <olayorijuinioluwa@gmail.com>
2025-07-22 07:50:29 +0100

    b2 edit: Update B2_REGION in .env Used New Method Forcefully ~ Which is the right way

--- utils/b2_signed_url.py
+++ utils/b2_signed_url.py
@@ -1,17 +1,28 @@
 import boto3
 import os
+from botocore.client import Config
+from functools import lru_cache
 
-def generate_b2_signed_url(key, expires_in=3600):
-    s3 = boto3.client(
+@lru_cache(maxsize=1)
+def get_b2_client():
+    return boto3.client(
         's3',
         endpoint_url='https://s3.us-west-004.backblazeb2.com',
         aws_access_key_id=os.environ['B2_KEY_ID'],
         aws_secret_access_key=os.environ['B2_APP_KEY'],
         region_name='us-west-004',
+        config=Config(signature_version='s3v4'),  # Force SigV4
     )
-    url = s3.generate_presigned_url(
-        'get_object',
-        Params={'Bucket': os.environ['B2_BUCKET_NAME'], 'Key': key},
-        ExpiresIn=expires_in,
-    )
-    return url
+
+def generate_b2_signed_url(key, expires_in=3600):
+    try:
+        s3 = get_b2_client()
+        url = s3.generate_presigned_url(
+            'get_object',
+            Params={'Bucket': os.environ['B2_BUCKET_NAME'], 'Key': key},
+            ExpiresIn=expires_in,
+        )
+        return url
+    except Exception as e:
+        # Optionally log the error
+        return None
